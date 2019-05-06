6c65c65913aae5961e1f5c30d552f740e085f5e2
AntoineBazin <36506539+AntoineBazin@users.noreply.github.com>
2019-05-06 10:16:08 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,36 @@
+// -----JS CODE-----
+// @input Component.FaceStretchVisual faceStretch
+
+var isStretched = false;
+var stretch = 0;
+var intensity = 0;
+
+function MouthOpened(eventData)
+{
+	if (isStretched)
+		stretch = -1;
+	else
+		stretch = 1;
+}
+
+function Update()
+{
+	if (stretch == 1)
+		intensity += getDeltaTime();
+	else if (stretch == -1)
+		intensity += getDeltaTime();
+	if (intensity >= 1) {
+		isStretched = true;
+		stretch = 0;
+	} else if (intensity <= 0) {
+		isStretched = false;
+		stretch = 0;
+	}
+	script.faceStretch.setFeatureWeight("Feature0", intensity);
+}
+
+var event = script.createEvent("MouthOpenedEvent");
+event.bind(MouthOpened);
+
+var update = script.createEvent("UpdateEvent");
+update.bind(Update);
